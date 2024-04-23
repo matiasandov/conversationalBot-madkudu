51ccdd0d5c946dd91bf8d72b02fca72afff8e9c4
matiasandov <a01422885@itesm.mx>
2024-04-23 13:25:34 +0200

    created new to branch to make repo public

---
+++ README.md
@@ -0,0 +1,20 @@
+# Madkudu Conversational Agent
+
+Dear Machine Learning team and recruiters at Madkudu,
+
+Welcome to my Conversational Agent project for Madkudu.
+
+The distribution of my repository is divided in 2:
+
+1. `solution`: In this directory you will find attached the final files with the solution to my project. Which are the followings:
+   
+  - 1.1 `madkudu_assistant_agent.py`: This is the most important file since it contains the Conversational Agent application.
+    -  1.1.1 To run this file remember to be in the  `solution` directory
+    -  1.1.2 Note that it is important to have accesss to a valid key of OpenAi, in this case I am leaving mine just for the team to test but after the review it will be deactivated.
+      - In this case I am using a free credits that I have from OpenAi.
+  
+  - 1.2  `dataExtraction.ipynb`: This file is a web crawler I built to extract data from some sections of the support website of madkud. Since I have limited credits of OpenAi I used few urls.
+  - 1.3 `all.txt`: Data extracted with the crawler
+  - 1.4 `assistant_creation.ipynb`: A notebook I used to test, scale, and create the conversational chain step by step. It also needs an OpenAi key
+
+2. `drafts_and_tests`: This directory contains different other solutions that I tried or that I can still use to scale this project. I attached this folder **just to show my creative process before achieving the final solution**. 
---
+++ solution/madkudu_assistant_agent.py
@@ -0,0 +1,149 @@
+#Langchain
+import os 
+# Hello dear recruiter if you would like to test this you can get a open ai gpt for free if you create 
+# a new account with a never seen number
+os.environ["OPENAI_API_KEY"] = ""
+from langchain_openai import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+
+
+#Receives name of file
+def load_and_process_data(name):
+
+    #1. Loading data
+    #using utf 8 since it has special characters
+    loader = TextLoader(name, encoding='utf-8')
+    documents = loader.load()
+
+    #2. Split data in chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        #i chose 500 because the data is not that numerous
+        # number of characters
+        chunk_size = 500, 
+        #How many chars are going to be overlapped in between each chunk
+        chunk_overlap = 250,
+        
+        #length function to measure each chunk based on its character count.
+        length_function = len,
+        
+        #includes index of character where the separation between each chunk is done
+        add_start_index = True
+        
+        )
+
+    chunks = text_splitter.split_documents(documents)
+
+ 
+    # 3. Embedding model
+    # Define the path to the pre-trained model you want to use
+    modelPath = "sentence-transformers/all-MiniLM-l6-v2"
+
+    # Create a dictionary with model configuration options, specifying to use the CPU for computations
+    model_kwargs = {'device':'cpu'}
+
+    # Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
+    encode_kwargs = {'normalize_embeddings': False}
+
+    # Initialize an instance of HuggingFaceEmbeddings with the specified parameters
+    embeddings = HuggingFaceEmbeddings(
+        model_name=modelPath,     # Provide the pre-trained model's path
+        model_kwargs=model_kwargs, # Pass the model configuration options
+        encode_kwargs=encode_kwargs # Pass the encoding options
+    )
+
+    #4. Embed chunks and store them in a FAISS vector store
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+
+    #5. Create a retriever for the vector store
+    data_retriever = vectorstore.as_retriever()
+    return data_retriever
+
+#big chain of little chains
+def start_chain(retriever):
+
+    #1. Instance our LLM
+    llm = ChatOpenAI(
+        #chose a cheap model to save credits
+        model = "gpt-3.5-turbo-0125",
+        #the closer to 0, the more factual the results are going to be
+        temperature = 0.3
+    )
+    
+   #2.1 Create Prompt containing Contextual knowledge retrived from the vectorstore
+    prompt = ChatPromptTemplate.from_messages(
+        [
+        ("system", "as customer support agent named Matias, respond precisely the user's questions based on the below context:\n\n{context}"),
+        # this is the variable that should be received where the conversation is going to be stored
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}")
+    ])
+    
+    #2.2 Chain to access to context
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    
+    #3.1 Prompt that is going to be able to answer the LAST MESSAGE given the history
+    last_msg_prompt = ChatPromptTemplate.from_messages([
+        
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Use the above conversation to generate a search query to look up in order to get information relevant to the conversation. If you do not find the answer mention the user to visit https://support.madkudu.com/hc/en-us ")
+    ])
+
+    #3.2 chain to answer past history and retrives similar vector from the database to the (context)
+    retriever_chain = create_history_aware_retriever(llm, retriever, last_msg_prompt)
+
+    #4. Conversational chain - join the two chains 
+    #   stores conversation and answers with context(RAG)
+    conversational_retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+    
+    return conversational_retrieval_chain
+
+def talk_with_bot(conversation_chain, question, chat_history):
+    answer = conversation_chain.invoke(
+       { 
+        "chat_history": chat_history,
+        "input": question
+        }
+    )
+    
+    return answer["answer"]
+
+if __name__ == "__main__":
+    #intialize chain
+    retriever = load_and_process_data('all.txt')
+    assistant_chain = start_chain(retriever)
+    
+    #array to store the messages
+    messages_historial = []
+    
+    start = True
+    
+    while True:
+        
+        if start:
+            print("-------------------------Welcome----------------------------")
+            print("Hello Duc and Ruben, my name is Matias and I am happy to apply to this internship. However, today I am going to be your customer service agent for Madkudu.")
+            print("Please feel free to ask me any question related to the urls I used in my data extraction crawler. ")
+            print("To finish the conversation type: bye ")
+            start = False
+        
+        user_message = input("You:")
+        if user_message.lower() == "bye":
+            print("See you soon!")
+            break
+        
+        answer = talk_with_bot(assistant_chain, user_message, messages_historial)
+        messages_historial.append(HumanMessage(content=user_message))
+        messages_historial.append(AIMessage(content=answer))
+        print("Matias: ", answer) 
+        
+        
